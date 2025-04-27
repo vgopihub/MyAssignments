e0ec6d4dc485664f15a7734e67b9843539305b23
vgopihub <vgopiraghavan@gmail.com>
2025-04-27 13:30:54 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version= 91.0f;
+		 String developer = "Google";
+		 boolean isBeta = false;
+		 int releaseYear = 2008;
+		 char shortcutKey = 'C';
+		 
+		 System.out.println(version);
+		 System.out.println(developer);
+		 System.out.println(isBeta);
+		 System.out.println(releaseYear);
+		 System.out.println(shortcutKey);
+	}
+
+}
